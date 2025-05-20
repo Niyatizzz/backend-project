32ecc47a1fa7f1d38b8f0bb316b5585eeb8ebc2e
Niyatizzz <niyatikolukula@gmail.com>
2025-05-20 17:32:31 +0530

    user reg and pass hash

--- mycontacts-backend/controller/userController.js
+++ mycontacts-backend/controller/userController.js
@@ -1,10 +1,35 @@
 const asyncHandler = require("express-async-handler")
+const bcrypt = require('bcrypt')
+const User = require("../models/userModel")
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ messege: "register the user" });
+    const {username, email, password} = req.body;
+    if(!username||!email||!password) {
+        res.status(400);
+        throw new Error("all fields are mandatory")
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable) {
+        res.status(400);
+        throw new Error("email already registered");
+    }
+
+    //hash pasword - using bcrypt library
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("hashed password : ", hashedPassword)
+
+    const user = await User.create({username, email, password: hashedPassword,})
+    console.log(`user created ${user}`)
+    if(user) {
+      res.status(201).json({_id: user.id, email: user.email})
+    } else {
+      res.status(400)
+      throw new Error("invalid credentials")
+    }
+    res.json({ messege: "register the user" });
 });
 
 //@desc Login a user
